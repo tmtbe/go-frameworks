c8cbcde24a699789b20bfcbad85a74ce6e27fc18
jincheng.zhang <jincheng.zhang@thoughtworks.com>
2021-12-29 16:08:46 +0800

    创建项目工程示例

--- internal/app/module1/interfaces/provider.go
+++ internal/app/module1/interfaces/provider.go
@@ -4,18 +4,18 @@ import (
 	"github.com/google/wire"
 	"go.uber.org/zap"
 	"test/internal/app/module1/application"
-	apis2 "test/internal/app/module1/interfaces/apis"
+	"test/internal/app/module1/interfaces/apis"
 	"test/internal/pkg/route"
 )
 
 // NewAPIS 这里注册API
 func NewAPIS(logger *zap.Logger, a *application.UserDetailApplication) []route.Controller {
 	var controllers []route.Controller
-	controllers = append(controllers, apis2.NewUserDetailAPI(logger, a))
+	controllers = append(controllers, apis.NewUserDetailAPI(logger, a))
 	return controllers
 }
 
 var ProviderSet = wire.NewSet(
 	NewAPIS,
-	apis2.CreateInitControllersFn,
+	apis.CreateInitControllersFn,
 )
